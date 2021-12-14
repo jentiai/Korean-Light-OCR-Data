7582e9ba6aa489223e3be2a2973539fd8ca50f51
eom-sooyeon <jk01027@naver.com>
2021-12-14 02:15:48 +0000

    [feat]:Add rotate and make gt

--- make_dataset.py
+++ make_dataset.py
@@ -1,34 +1,119 @@
 import os
 import shutil
+import time
+import json
+import copy
 import argparse
 
-def make_test_image(image_path, test_image_list_path, output):
-    f = open(os.path.join(test_image_list_path, 'aihub_test_image_list.txt'), 'r', encoding='UTF8')
+from collections import defaultdict
+from utils.rotate import check_word_include_char, generate_rbox
 
-    test_image_list = []
-    for line in f.readlines():
-        test_image_list.append(line.strip())
+DEFAULT_AIHUB_ANNOTATION_FILE = 'textinthewild_data_info.json'
 
-    os.makedirs(os.path.join(output, 'aihub_test'))
-    aihub_class = ['book', 'Goods', 'Singboard', 'Traffic_Sign']
-    for acls in aihub_class:
-        dir_name = os.path.join('aihub_test', acls)
-        os.makedirs(os.path.join(output, dir_name))
+class AihubTest:
+    def __init__(self, aihub_path, test_list_path, rotate=True, margin=5):
+        self.aihub_json = json.load(open(os.path.join(aihub_path, DEFAULT_AIHUB_ANNOTATION_FILE), 'r', encoding='UTF8'))
+        self.test_list_file = open(os.path.join(test_list_path, 'aihub_test_image_list.txt'), 'r', encoding='UTF8')
 
-    for (path, dir, files) in os.walk(image_path):
-        files = list(files)
-        
-        for file in files:
-            test_image_name = os.path.basename(path) + '/' + file
-            if test_image_name in test_image_list:
-                base_dir = os.path.join(output, 'aihub_test')
-                shutil.copy(os.path.join(path, file), os.path.join(base_dir, test_image_name))
+        self.aihub_test_dict, self.aihub_word_list, self.aihub_char_list, self.aihub_ignored_list = defaultdict(list), defaultdict(list), defaultdict(list), defaultdict(list)
+
+        self.test_list = []
+        for line in self.test_list_file.readlines():
+            self.test_list.append((line.strip()).split('/')[-1])
+
+        print('initialization...')
+        for anno in self.aihub_json['annotations']:
+            self.aihub_test_dict[anno['image_id']].append(anno)
+            if anno['attributes']['class'] == 'character':
+                self.aihub_char_list[anno['image_id']].append(anno)
+            elif anno['attributes']['class'] == 'word':
+                self.aihub_word_list[anno['image_id']].append(anno)
+            elif anno['attributes']['class'] == 'ignored':
+                self.aihub_ignored_list[anno['image_id']].append(anno)
+
+        if rotate:
+            print(f'make rotate annotations with margin {margin}...')
+            for image in self.aihub_json['images']:
+                self.add_rotated_boxes(image['id'], margin)
+
+    def add_rotated_boxes(self, img_id, margin):
+        anno_words = self.aihub_word_list[img_id]
+        anno_chars = self.aihub_char_list[img_id]
+
+        for anno_word in anno_words:
+            anno_chars_in_w = []
+            for anno_char in anno_chars:
+                if check_word_include_char(anno_word['bbox'], anno_char['bbox'], margin):
+                    anno_chars_in_w.append(anno_char)
+            if len(anno_chars_in_w)==0:
+                x, y, w, h = anno_word['bbox']
+                anno_rbox = copy.deepcopy(anno_word)
+
+                poly = [x,y, x+w,y, x+w,y+h, x,y+h]
+                anno_rbox["poly"] = poly
+            else:
+                anno_rbox = generate_rbox(anno_word, anno_chars_in_w)
+            
+            self.aihub_ignored_list[img_id].append(anno_rbox)
+
+    def calc_wh(self, poly):
+        min_x, max_x = min(poly[0::2]), max(poly[0::2])
+        min_y, max_y = min(poly[1::2]), max(poly[1::2])
+        width = max_x - min_x
+        height = max_y - min_y
+
+        if width == 0 or height == 0:
+            return True
+        else:
+            return False
+
+    def make_rotate_gt(self, image_id, file):
+        for annot in self.aihub_ignored_list[image_id]:
+            if annot['text'] is not None and annot['text'].strip() == "":
+                continue
+
+            if annot['attributes']['class'] == 'rbox' or annot['attributes']['class'] == 'word':
+                poly = annot['poly']
+            elif annot['attributes']['class'] == 'ignored':
+                [bbox_x, bbox_y, bbox_w, bbox_h] = annot['bbox']
+                poly = [bbox_x, bbox_y, bbox_x+bbox_w, bbox_y, bbox_x+bbox_w, bbox_y+bbox_h, bbox_x, bbox_y+bbox_h]
+
+            if self.calc_wh(poly):
+                continue
+
+            for p in poly:
+                file.write(str(int(p))+', ')
+
+            if annot['text'] is None: # ignored
+                file.write(str('###'))
+            else:
+                file.write(str(annot['text']))
+            file.write('\n')
+
+
+    def store_gt(self, gt_path, rotate):
+        os.makedirs(os.path.join(gt_path, 'gt'), exist_ok=True)
+
+        for image in self.aihub_json['images']:
+            if image['file_name'] in self.test_list:
+                rf = open(os.path.join(gt_path, 'gt/{}.txt'.format(image['file_name'].split('.jpg')[0])), 'w', encoding='UTF8')
+                if rotate:
+                    self.make_rotate_gt(image['id'], rf)
+                else:
+                    self.make_general_gt(image['id'], rf)
+                rf.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--aihub', '-a', type=str, default = './', help='aihub images path')
     parser.add_argument('--list_path', '-l', type=str, default = './', help='aihub test image list txt file path')
     parser.add_argument('--output', '-o', type=str, default = './', help='output path')
+    parser.add_argument('--rotate', action='store_true')
+    parser.add_argument('--margin', type=int, default=5)
     args = parser.parse_args()
 
-    make_test_image(args.aihub, args.list_path, args.output)
+    aihub_test = AihubTest(args.aihub, args.list_path, args.rotate, args.margin)
+
+    print('make gt files...')
+    aihub_test.store_gt(args.aihub, args.rotate)
+    # make_test_image(args.aihub, args.list_path, args.output)
