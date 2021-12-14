fccb1dfb2a74885bc09aec395774e7b7061f8a62
eom-sooyeon <jk01027@naver.com>
2021-12-14 02:53:38 +0000

    [feat]:Add post process

--- make_dataset.py
+++ make_dataset.py
@@ -7,6 +7,7 @@ import argparse
 
 from collections import defaultdict
 from utils.rotate import check_word_include_char, generate_rbox
+from utils.post_processor import remove_labels_no_image
 
 DEFAULT_AIHUB_ANNOTATION_FILE = 'textinthewild_data_info.json'
 
@@ -109,6 +110,30 @@ class AihubTest:
                 self.make_rotate_gt(image['id'], rf)
                 rf.close()
 
+def remove_no_gt(parent_dir):
+    deleted_gt = []
+
+    for path, dir, files in os.walk(parent_dir):
+        if not files: continue
+
+        for name in files:
+            gt_path = os.path.join(path, name)
+
+            # remove duplicated name
+            if name == '00C6DD18C320D0A8E8E26AFA84AB5555.txt' or name == '00F3E524ED9EC2FFD20BC6156EDF5BE3.txt':
+                deleted_gt.append(gt_path)
+                os.remove(gt_path)
+                continue
+
+            # remove empty gt
+            with open(gt_path, "r", encoding="utf-8") as f:
+                lines = f.read()
+            if not lines:
+                deleted_gt.append(gt_path)
+                os.remove(gt_path)
+
+    return 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--aihub', '-a', type=str, default = './', help='aihub images path')
@@ -121,4 +146,6 @@ if __name__ == "__main__":
 
     print('make gt files...')
     aihub_test.store_gt(args.output)
-    # make_test_image(args.aihub, args.list_path, args.output)
+
+    print('remove gt with no annotation')
+    deleted_gt = remove_no_gt(os.path.join(args.aihub, 'gt'))
